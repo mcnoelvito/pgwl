5ea699f2c00044a5fe4c93fc11695c0f1c0b602b
mcnoelvito <noelvitohu@gmail.com>
2024-06-13 08:39:54 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <h1 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Explore KWB with Mine') }}
+        </h1>
     </x-slot>
 
 
