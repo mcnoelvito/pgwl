a5f399825693084ab32635e455fdc7da8fa3e2fa
mcnoelvito <noelvitohu@gmail.com>
2024-06-13 11:38:35 +0700

    update

--- resources/views/index-public.blade.php
+++ resources/views/index-public.blade.php
@@ -11,14 +11,14 @@
 
         #map {
             height: calc(100vh - 56px);
-            width: 100%
+            width: 100%;
             margin: 0;
         }
     </style>
 @endsection
 
 @section('content')
-    <div id="map" style="width: 100vw; height: 100vh; margin: 0"></div>
+    <div id="map" style="width: 100%; height: calc(100vh - 56px); margin: 0"></div>
 
 @endsection
 
@@ -115,12 +115,46 @@
             polygon.addData(data);
             map.addLayer(polygon);
         });
+
+        //Basemap
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+        'Map data 0 <a href="https: //www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }). addTo(map);
+        var basemap1 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | <a href="DIVSIG UGM" target="_blank">DIVSIG UGM</a>' //menambahkan nama//
+        });
+        var basemap2 = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{ z }/{ y }/{ x }',
+            {
+                attribution: 'Tiles &copy; Esri | <a href="WebGIS Kota Batu" target="_blank">DIVSIG UGM</a>'
+            });
+        var basemap3 = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{ z }/{ y }/{ x }',
+            {
+                attribution: 'Tiles &copy; Esri | <a href="WebGIS Kota Batu" target="_blank">DIVSIG UGM</a>'
+            });
+        var basemap4 = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+            attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        });
+
+
+        basemap1.addTo(map);
+
     // layer control
-var overlayMaps = {
-    "Point": point,
-    "Polyline": line,
-    "Polygon": polygon
-};
+    var baseMaps = {
+            "OpenStreetMap": basemap1,
+            "Esri World Street": basemap2,
+            "Esri Imagery": basemap3,
+            "Stadia Dark Mode": basemap4
+
+        };
+    var overlayMaps = {
+            "Point": point,
+            "Polyline": polyline,
+            "Polygon": polygon
+        };
+
+        var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
 
     //Watermark
     L.Control.Watermark = L.Control.extend({
@@ -137,6 +171,6 @@ var overlayMaps = {
         }
         L.control.watermark({ position: 'bottomright' }).addTo(map);
 
-var layerControl = L.control.layers(null, overlayMaps).addTo(map);
+
 </script>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -368,14 +368,16 @@
         var basemap4 = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
             attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         });
+
         basemap1.addTo(map);
 
-        //layer control
-        var baseMaps = {
+    // layer control
+    var baseMaps = {
             "OpenStreetMap": basemap1,
             "Esri World Street": basemap2,
             "Esri Imagery": basemap3,
             "Stadia Dark Mode": basemap4
+            
         };
         var overlayMaps = {
             "Point": point,
